f29545ae11a86ae8ef59e0c573e550b71f705ee9
Luiz Francisco <luizfk.marinho@gmail.com>
2025-03-29 18:21:42 -0300

    feat: add main function

--- webscrapping/main.py
+++ webscrapping/main.py
@@ -1,6 +1,25 @@
 from scraper import Scrapping
 
-url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
-scraper = Scrapping(url)
+def main():
+    url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 
-print("Links anexos: ", scraper.get_anexos_links())
+    config = {
+        'download': True,
+        'compactar': True,
+        'anexos_dir': 'anexos',
+        'zip_name': 'anexos_ans.zip',
+        'remove_after': True
+    }
+
+    scraper = Scrapping(url)
+    anexos, zip_path = scraper.processar_anexos(**config)
+
+    print(f"{len(anexos)} anexos baixados com sucesso")
+    if zip_path and isinstance(zip_path, str):
+        print(f" - Endereço dos arquivos: {zip_path}")
+    
+    elif isinstance(zip_path, str):
+        print(f"- Erro na compactação: {zip_path}")
+
+if __name__ == "__main__":
+    main()
